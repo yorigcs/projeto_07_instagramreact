8587908e7f6521f435ff19dfb69a85fa7def5dd4
Yorí <yorigcsdev@gmail.com>
2022-05-08 04:38:54 -0300

    Implementacao do bonus

--- src/Componentes/mainContent/leftContent/posts/Posts.js
+++ src/Componentes/mainContent/leftContent/posts/Posts.js
@@ -1,72 +1,84 @@
 import './posts.css';
+import React from 'react';
 
-const usersPosts = [
-    {username: 'meowed', userimage: 'meowed', userpost: 'gato-telefone', id: 0},
-    {username: 'barked', userimage: 'barked', userpost: 'dog', id: 1}
-];
-
-const likedPosts = [
-    {mainLiked: 'respondeai', imageMainLiked: 'respondeai', allLikes: 101.523},
-    {mainLiked: 'adorable_animals', imageMainLiked: 'adorable_animals', allLikes: 99.159}
-];
 
 export default function Posts() {
+    const usersPosts = [
+        {username: 'meowed', userimage: 'meowed', userpost: 'gato-telefone',
+        actions: {likedBy: 'respondeai', image: 'respondeai', allLikes: 101.523}, id: 0},
+
+        {username: 'barked', userimage: 'barked', userpost: 'dog',
+        actions: {likedBy: 'adorable_animals', image: 'adorable_animals', allLikes: 99.159}, id: 1}
+    ];
+    
     return (
         <div className='posts'>
             {usersPosts.map( user => (
 
                 <div className='post' key={user.id}>
-                    <Post {...user} />
-
-                    <div className='fundo' key={likedPosts[user.id]}>
-                        <Fundo {...likedPosts[user.id]} />
-                    </div>
+                    <Post {...user} />            
                 </div>
             ))}
         </div>
     );
 }
 
-const Post = ({username,userimage,userpost}) => (
-    <div >
-        <div className='topo'>
-            <div className='usuario'>
-                <img src={`img/${userimage}.svg`} alt={userimage}></img>
-                {username}
-            </div>
+const Post = ({username,userimage,userpost,actions}) => {
+    const [like,setLike] = React.useState(["black","heart-outline"]);
 
-            <div className="acoes">
-                  <ion-icon name="ellipsis-horizontal"></ion-icon>
+    function getLike(clickImage = '') {
+        if(clickImage === 'clickImage'){
+            if(like[1] === 'heart') {
+                return;
+            }
+            setLike(["red","heart"]);
+        } else {
+            if(like[1] === 'heart') {
+                setLike(["black","heart-outline"]);
+            } else {
+                setLike(["red","heart"]);
+            }
+        }   
+    }
+    return (
+        <div >
+            <div className='topo'>
+                <div className='usuario'>
+                    <img src={`img/${userimage}.svg`}  alt={userimage}></img>
+                    {username}
+                </div>
+    
+                <div className="acoes">
+                      <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
             </div>
+    
+            <div className="conteudo">
+                <img src={`img/${userpost}.svg`} onClick={() => getLike('clickImage')} alt={userpost}></img>
+            </div>
+    
+            <div className='fundo'>
+                <div className="acoes">
+                    <div>
+                        <ion-icon style={{color:like[0]}} name={like[1]} onClick={getLike}></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <ion-icon name="bookmark-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div className="curtidas">
+                <img src={`img/${actions.image}.svg`} alt={actions.image}></img>
+                    <div className="texto">
+                        Curtido por <strong>{actions.likedBy}</strong> e <strong>outras {actions.allLikes} pessoas</strong>
+                    </div>
+                </div>        
+            </div>
+    
         </div>
-
-        <div className="conteudo">
-            <img src={`img/${userpost}.svg`} alt={userpost}></img>
-        </div>
-
-    </div>
-);
+    );
+};
 
 
-const Fundo = ({mainLiked,imageMainLiked,allLikes}) => (
-    <div>
-        <div className="acoes">
-            <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-
-            <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-            </div>
-        </div>
-
-        <div className="curtidas">
-        <img src={`img/${imageMainLiked}.svg`} alt={imageMainLiked}></img>
-            <div className="texto">
-                Curtido por <strong>{mainLiked}</strong> e <strong>outras {allLikes} pessoas</strong>
-            </div>
-        </div>
-    </div>
-);
